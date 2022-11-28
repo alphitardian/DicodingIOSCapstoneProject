a0b5a8e14d12c67d2b1c1648ff63ba6bc1bba5c1
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2022-11-28 09:55:58 +0700

    Initial Commit

---
+++ DicodingCapstoneProject/DicodingCapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicodingCapstoneProjectApp.swift
+//  DicodingCapstoneProject
+//
+//  Created by Ardian Pramudya Alphita on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DicodingCapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
